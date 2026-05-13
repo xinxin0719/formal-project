3d4cdd7fa6cd12ecac2f38ad3d3bda5a198eb55c
Guo Chencin <1833185711@qq.com>
2026-05-13 23:40:50 +0800

    0.0.1

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('respons');
+})
+
+server.listen(3000, () => {
+    console.log('succeed');
+})
