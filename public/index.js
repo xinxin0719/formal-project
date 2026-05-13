852682a3e15aed41b44b36fd8b954551d8ccf93a
Guo Chencin <1833185711@qq.com>
2026-05-14 00:12:50 +0800

    0.0.1

--- public/index.js
+++ public/index.js
@@ -1,1 +1,4 @@
 const socket = io();
+
+const radius = document.querySelector('.main').clientWidth * 0.5;
+document.querySelector('.main').style.borderRadius = `${radius}px 0 / ${radius}px ${radius}px`;
