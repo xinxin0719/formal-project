8e466dffc3233ff1a8cc08fff6787466abc290de
xinxin0719 <1833185711@qq.com>
2026-05-15 02:55:42 +0000

    0.0.2

--- public/index.js
+++ public/index.js
@@ -2,3 +2,8 @@ const socket = io();
 
 const radius = document.querySelector('.main').clientWidth * 0.5;
 document.querySelector('.main').style.borderRadius = `${radius}px 0 / ${radius}px ${radius}px`;
+
+document.querySelector('.submit').addEventListener('click', () => {
+    const name = document.querySelector('.name').value;
+    socket.emit('nameInput', name);
+});
