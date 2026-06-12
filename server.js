68e1f43841063547d0a8ce5b53e74b5e17d052cb
Guo Chencin <1833185711@qq.com>
2026-06-12 17:24:33 +0800

    0.0.5

--- server.js
+++ server.js
@@ -6,10 +6,29 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const fs = require('fs');
+const fileUrl = './data/users.json';
+
 app.use(express.static('public'));
+
+let fileContent = fs.readFileSync(fileUrl, 'utf-8');
+if (!fileContent.trim()) {
+    fs.writeFileSync(fileUrl, JSON.stringify([], null, 2));
+}
 
 io.on('connection', (socket) => {
     console.log('response');
+    socket.on('nameInput', (name) => {
+        let users = JSON.parse(fs.readFileSync(fileUrl, 'utf-8'));
+        let user = users.find(item => item.name === name);
+        if (user) {
+
+        } else {
+            users.push({ name: name});
+            fs.writeFileSync(fileUrl, JSON.stringify(users, null, 2))
+        }
+        console.log(name);
+    })
 })
 
 server.listen(3000, () => {
